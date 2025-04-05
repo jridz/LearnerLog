2ea873e07abc020f46224bbe04c9399dc57485b6
Equinoxx4978 <joshuaridley07@gmail.com>
2025-04-06 09:53:34 +1000

    Add importing and exporting backups

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ import {createPastSessionItem} from "./scripts/components/pastSessionItem.js";
 import {deleteSession} from "./scripts/components/editSessionModal.js";
 import {clearAppData} from "./scripts/functions/clearAppData.js";
 import {clearAppCache} from "./scripts/functions/clearAppCache.js";
+import {toast} from "./scripts/components/toast.js";
+import {showConfirmationDialog} from "./scripts/functions/showConfirmationDialog.js";
 
 const newSessionForm = document.getElementById("newSessionForm");
 const newSessionModal = document.getElementById("newSessionModal");
@@ -92,6 +94,55 @@ export function renderPastSessions() {
   });
 }
 
+function exportData() {
+  const sessions = getAllStoredSessions();
+  const dataStr = JSON.stringify(sessions);
+  const dataUri = 'data:application/json;charset=utf-8,' + encodeURIComponent(dataStr);
+  const exportTime = new Date().toISOString().replace(/[:\-T]/g, '_').split('.')[0];
+
+  const exportFileDefaultName = `learnerlog_backup_${exportTime}.json`;
+
+  const linkElement = document.createElement('a');
+  linkElement.setAttribute('href', dataUri);
+  linkElement.setAttribute('download', exportFileDefaultName);
+  linkElement.click();
+
+  toast("Backup exported", "green");
+}
+
+function importData(files) {
+  const file = files[0];
+  if (!file) {
+    toast("No file selected", "#ff0000");
+    return;
+  }
+
+  const reader = new FileReader();
+  reader.onload = (event) => {
+    try {
+      const sessions = JSON.parse(event.target.result);
+      if (!Array.isArray(sessions)) {
+        toast("Invalid file format", "#ff0000");
+        return;
+      }
+      // Ask to confirm the import
+      showConfirmationDialog("Are you sure you want to import this backup? THIS WILL OVERWRITE YOUR CURRENT DATA.", function (confirmed) {
+        // Handle confirmation
+        if (confirmed) {
+          window.localStorage.setItem(STORAGE_KEY, JSON.stringify(sessions));
+          toast("Backup imported", "green");
+        } else {
+          toast("Import cancelled", "red");
+        }
+      });
+    } catch (error) {
+      toast("Error parsing file", "#ff0000");
+    }
+  };
+
+  reader.readAsText(file);
+}
+
 // Register the service worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
@@ -112,3 +163,6 @@ window.resetErrors = resetErrors;
 window.deleteSession = deleteSession;
 window.clearAppData = clearAppData;
 window.clearAppCache = clearAppCache;
+window.exportData = exportData;
+window.importData = importData;
+window.exportData = exportData;
